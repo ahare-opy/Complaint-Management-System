06fce8c18eb8e7c2ecbdd52e185060ae897dcd3d
TanvirYeasinOpy <tanvir016opy@gmail.com>
2022-10-27 15:52:18 +0600

    Initial setup by opy

---
+++ nsu_ovijog_server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const next = require('next');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dev = process.env.NODE_ENV != 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+require('dotenv').config({ path: './config.env' });
+const connectDB = require('./utilsServer/connectDB');
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+
+nextApp.prepare().then(() => {
+  app.use(require('cookie-parser'));
+
+  /*app.use('/api/v1/signup', require('./api/signupRoute'));
+  app.use('/api/v1/login', require('./api/loginRoute'));
+  app.use('/api/v1/user', require('./api/userRoute'));
+  app.use('/api/v1/complain', require('./api/complainRoute'));
+  app.use('/api/v1/review', require('./api/reviewRoute'));
+  app.use('/api/v1', loginRoute);
+  app.use('/api/v1/dashboard', dashboardRoute);
+  app.use('/api/v1/signupTwo', signupTwoRoute);
+  app.use('/api/v1/user', userRoute);
+  app.use('/api/v1/complain', complainRoute);
+  app.use('/api/v1/review', reviewRoute);
+  app.use('/api/v1/systemAdmin', systemAdminRoute);*/
+
+  app.all('*', (req, res) => handle(req, res));
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Express server running on ${PORT}`);
+  });
+});
