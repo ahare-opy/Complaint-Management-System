6761f8458393f73ae7972405fefe3684c36c45fd
TanvirYeasinOpy <tanvir016opy@gmail.com>
2022-12-21 16:37:35 +0600

    Chnages according to sir.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7857ba52a9693ada"; }
+/******/ 		__webpack_require__.h = function() { return "e88fac03087b817f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
